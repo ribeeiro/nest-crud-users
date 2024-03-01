d6e8e565f63e10505e799d41157b18fb5c1d5b02
Thiago Gonçalves Ribeiro <thiago.ribeiro@oabrj.org.br>
2024-03-01 16:31:53 -0300

    implement user createdAt, updatedAt & removedAt

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,8 +1,15 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity({ name: 'users' })
 export class UserEntity {
-  @PrimaryColumn('uuid')
+  @PrimaryGeneratedColumn('uuid')
   id: number;
 
   @Column()
@@ -14,5 +21,12 @@ export class UserEntity {
   @Column()
   age: number;
 
-  // TODO: createdAt, updatedAt & deletedAt
+  @CreateDateColumn()
+  created_at: string;
+
+  @UpdateDateColumn()
+  updated_at: string;
+
+  @DeleteDateColumn()
+  deleted_at: string;
 }
