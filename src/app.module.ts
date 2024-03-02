ece764c6af399487705195e22e17de9459751e1e
Thiago G Ribeiro <thiagogr71@gmail.com>
2024-03-02 11:26:45 -0300

    create dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,18 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './users/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'users',
+      host: process.env.CONNECTION_HOST,
+      port: Number(process.env.CONNECTION_PORT),
+      username: process.env.CONNECTION_USERNAME,
+      password: process.env.CONNECTION_PASSWORD,
+      database: process.env.CONNECTION_DATABASE,
       entities: [join(__dirname, '**', '*.entity.{ts,js}')],
       synchronize: true,
     }),
